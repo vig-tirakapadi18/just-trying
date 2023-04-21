ffc53ec1e2cf497bdc5d4bf330160b90d075913c
vig-titakapadi18 <vighneshvt2000@gmail.com>
2023-04-21 21:47:20 +0530

    added map using Leaflet package

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -16,6 +16,7 @@ import { HiLocationMarker } from "react-icons/hi";
 import { FaBath, FaBed, FaChair, FaParking } from "react-icons/fa";
 import { getAuth } from "firebase/auth";
 import Contact from "../components/Contact";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 const Listing = () => {
   const [listing, setListing] = useState(null);
@@ -143,7 +144,24 @@ const Listing = () => {
             />
           )}
         </div>
-        <div className="bg-blue-300 w-full h-[200px] lg-[400px] z-10 overflow-x-hidden"></div>
+        <div className="w-full mt-6 h-[200px] md:h-[400px] z-10 overflow-x-hidden md:mt-0 md:ml-2">
+          <MapContainer
+            center={[listing.geolocation.lat, listing.geolocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}
+            style={{ height: "100%", width: "100%" }}>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker
+              position={[listing.geolocation.lat, listing.geolocation.lng]}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </div>
       </div>
     </main>
   );
